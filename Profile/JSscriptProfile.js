c1b3166a0a2fa93d70b11ef85e37bf09e7c07ab4
danys649 <dfurkol653@gmail.com>
2023-11-09 12:21:36 +0200

    0.1

--- Profile/JSscriptProfile.js
+++ Profile/JSscriptProfile.js
@@ -1,29 +1,51 @@
-const editButton = document.getElementById("editButton");
-const saveButton = document.getElementById("saveButton");
-const inputFields = document.querySelectorAll('input[type="text"]');
-const dateField = document.getElementById("dob");
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const saveButton = document.getElementById("saveButton");
+  saveButton.addEventListener("click", () => {
+    let lastName = document.getElementById("lastName");
+    let firstName = document.getElementById("firstName");
+    let middleName = document.getElementById("middleName");
+    let dob = document.getElementById("dob");
+    let city = document.getElementById("city");
+    let street = document.getElementById("street");
+    let houseNumber = document.getElementById("houseNumber");
+    let apartmentNumber = document.getElementById("apartmentNumber");
+    let male = document.getElementById("male");
+    let female = document.getElementById("female");
 
-editButton.addEventListener("click", () => {
-  inputFields.forEach((input) => {
-    input.removeAttribute("readonly");
-  });
-  dateField.removeAttribute("disabled");
-  checkboxes.forEach((checkbox) => {
-    checkbox.removeAttribute("disabled");
-  });
-  editButton.style.display = "none";
-  saveButton.style.display = "block";
-});
+    let fields = [
+      lastName,
+      firstName,
+      middleName,
+      dob,
+      city,
+      street,
+      houseNumber,
+      apartmentNumber,
+    ];
 
-saveButton.addEventListener("click", () => {
-  inputFields.forEach((input) => {
-    input.setAttribute("readonly", true);
+    let allFilled = true;
+
+    fields.forEach((field) => {
+      if (!field.value) {
+        field.style.borderColor = "red";
+        allFilled = false;
+      } else {
+        field.style.borderColor = "";
+      }
+    });
+
+    if (!male.checked && !female.checked) {
+      male.style.outline = "1px solid red";
+      female.style.outline = "1px solid red";
+      allFilled = false;
+    } else {
+      male.style.outline = "";
+      female.style.outline = "";
+    }
+
+    if (allFilled) {
+      document.body.innerHTML = ""; // Удалить текущий HTML
+      location.href = "Home.html"; // Перейти на новую страницу 'Home.html'
+    }
   });
-  dateField.setAttribute("disabled", true);
-  checkboxes.forEach((checkbox) => {
-    checkbox.setAttribute("disabled", true);
-  });
-  editButton.style.display = "block";
-  saveButton.style.display = "none";
 });
